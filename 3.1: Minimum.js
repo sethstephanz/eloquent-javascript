0b72ad24ae0c78b307c5b73386e2a7216e6a3ecc
Seth <sethstephanz@gmail.com>
2021-01-12 12:27:03 -0500

    Update and rename minimum.js to 3.1: Minimum.js

---
+++ 3.1: Minimum.js
@@ -0,0 +1,5 @@
+function min (n, m) {
+  return Math.min(n, m);
+}
+
+min(-200, 1000; //--> -200
--- minimum.js
+++
@@ -1,6 +0,0 @@
-function min (n, m) {
-  return Math.min(n, m);
-}
-
- //Sample input: -200, 100;
- //Expected output: -200
