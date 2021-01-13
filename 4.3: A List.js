7c4a8b19f8ec8b8752d77ab47019379f2c762cdd
Seth <sethstephanz@gmail.com>
2021-01-13 13:29:49 -0500

    Update 4.3: A List.js

--- 4.3: A List.js
+++ 4.3: A List.js
@@ -1,6 +1,6 @@
 //Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument.
 
-function arrayToList(array) {
+arrayToList = array => {
   let list = {};
   for (let i = array.length - 1; i >= 0; i--) {
     list = {
@@ -13,7 +13,24 @@ function arrayToList(array) {
 
 arrayToList([1, 2, 3]) //--> { value: 1, rest: { value: 2, rest: { value: 3, rest: {} } } }
 
-//Also write a listToArray function that produces an array from a list. Then add a helper function prepend,
-//which takes creates a new list that adds the element to the front of the input list, and nth, which takes
-//a list and a number and returns the element at the given position in the list (with zero referring to the first element)
-//or undefined when there is not such element. If you haven't already, also write a recursive version of nth.
+//Also write a listToArray function that produces an array from a list. 
+
+let list = {1: 'A', 2: 'B', 3: 'C'};
+
+listToArray = list => {
+  let arr = [];
+  for (let i in list) {
+    arr.push(`${i}: ${list[i]}`);
+  }
+  return arr;
+}
+
+listToArray(list); //--> [ '1: A', '2: B', '3: C' ]
+
+//Then add a helper function prepend, which takes creates a new list that adds the element to the front of the input list, 
+//and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring to the first element)
+//or undefined when there is no such element. 
+
+
+
+//If you haven't already, also write a recursive version of nth.
