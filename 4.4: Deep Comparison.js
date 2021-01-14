1d9fba5554157f7274c29bda972fdec3b24135d5
Seth <sethstephanz@gmail.com>
2021-01-14 12:15:01 -0500

    Create 4.4: Deep Comparison.js

---
+++ 4.4: Deep Comparison.js
@@ -0,0 +1,2 @@
+//Write a function, deepEqual, that takes two values and returns true only if they are the same value or are objects with the same properties,
+//where the values of the properties are equal when compared with a recursive call to deepEqual.
